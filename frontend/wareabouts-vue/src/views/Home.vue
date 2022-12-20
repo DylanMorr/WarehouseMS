2ca74799b743889f6c3980c36c271ac501b885c7
DylanMorr <G00365708@gmit.ie>
2022-12-20 15:15:45 +0000

    Basic vue sidebar and pages frontend

---
+++ frontend/wareabouts-vue/src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <main class="home-page">
+        <h1>Home Page</h1>
+        <p>This is home page</p>
+    </main>
+</template>
---
+++ frontend/wareabouts-vue/src/views/Product.vue
@@ -0,0 +1,6 @@
+<template>
+    <main class="product-page">
+        <h1>Product Page</h1>
+        <p>This is product page</p>
+    </main>
+</template>
